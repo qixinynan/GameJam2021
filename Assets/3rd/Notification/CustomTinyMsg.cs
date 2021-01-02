a43a1e4d84170d95f1e9302ac4390cdecba83f8d
mage <849166805@qq.com>
2021-01-02 09:32:16 +0800

    add new content

---
+++ Assets/3rd/Notification/CustomTinyMsg.cs
@@ -0,0 +1,15 @@
+﻿using TinyMessenger;
+
+public enum CommonStatus {
+    Show = 0,
+    Close
+}
+
+public class ConmonMessage : ITinyMessage {
+    public object Sender { get; private set; }
+    public CommonStatus curStatus;
+
+    public ConmonMessage(CommonStatus status) {
+        curStatus = status;
+    }
+}
