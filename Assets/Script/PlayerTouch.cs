0c239eb0d57d252a34c0458d8b366cc37359851a
mage <849166805@qq.com>
2021-01-28 23:45:32 +0800

    add watch

---
+++ Assets/Script/PlayerTouch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTouch : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("交互钟表");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("取消交互钟表");
+    }
+}
