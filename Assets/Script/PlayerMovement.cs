dc400c68864f74692e9edc177925c2ff204e7a4d
BCSX <3185835784@qq.com>
2021-01-29 22:59:09 +0800

    Player Move 2.0

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.Serialization;
+
 //using DG.Tweening;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject playerA;
+    public GameObject playerB;
     public float speed;
     public static PlayerMovement instance;
+    public CinemachineVirtualCamera virtualCamera;
     private Vector3 clickPos;
     private bool isMoving = false;
+    private bool isControllA = true;
     
     private void Awake()
     {
@@ -26,32 +32,67 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        PlayerControllUpdate();
+        if (isControllA)
         {
-            /*RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (hit.collider != null)
-            {
-                Debug.Log(hit.transform.name);
-                if (hit.transform.CompareTag(Util.TagCollection.stopPos))
-                {
-                    Debug.Log("click : " + hit.transform.name);
-                    return;
-                }
-            }*/
-            
-
-            clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            clickPos.z = player.transform.position.z;
-            player.GetComponent<NavMeshAgent2D>().destination = clickPos;
-            //player.GetComponent<Rigidbody2D>().velocity = Vector3.Normalize(clickPos - player.transform.position) * speed;
-            //isMoving = true;
+            PlayerMove(playerA);
+        }
+        else
+        {
+             PlayerMove(playerB);
         }
 
-        // if (Vector3.Distance(player.transform.position, clickPos) <= 0.1f && isMoving)
-        // {
-        //     player.transform.position = clickPos;
-        //     player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        //     isMoving = false;
-        // }
+    }
+
+    void PlayerMove(GameObject player)
+    {
+        Rigidbody2D rig = player.GetComponent<Rigidbody2D>();
+        float xMove = 0;
+        float yMove = 0;
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            yMove = speed;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            yMove = -speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            xMove = -speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            xMove = speed;
+        }
+        if (yMove != 0)
+        {
+            rig.velocity = new Vector2(rig.velocity.x, yMove);
+            isMoving = true;
+        }
+        if(xMove != 0)
+        {
+            rig.velocity = new Vector2(xMove,rig.velocity.y);
+            isMoving = true;
+        }
+        if (xMove == 0 && yMove == 0)
+        {
+            rig.velocity = new Vector2(0,0);
+        }
+    }
+
+    void PlayerControllUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ControllChange();
+        }
+    }
+
+    void ControllChange()
+    {
+        isControllA = !isControllA;
+        virtualCamera.Follow = isControllA ? playerA.gameObject.transform : playerB.gameObject.transform;
     }
 }
